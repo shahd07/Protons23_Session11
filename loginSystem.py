a0eb8b863c150c03a676e684c1ede53a7c50dc89
fayhaafifol <fayhaaalsakkar@gmail.com>
2023-08-22 04:13:41 +0300

    shahdod and fayhaa were here

--- loginSystem.py
+++ loginSystem.py
@@ -1,4 +1,5 @@
-# Define constants for messages and choices
+# shahdod and fayhaa were here
+#we want hohos
 USERNAME_MESSAGE = "Username: "
 PASSWORD_MESSAGE = "Password: "
 SECRET_MESSAGE = "Now enter your secret phrase for safekeeping: "
@@ -9,60 +10,106 @@ EXIT_CHOICE = "3) Exit"
 # Initialize an empty dictionary as the database
 database = {}
 
-# Define a function to check if a username exists in the database
+
 def username_exists(username):
+
     return username in database
 
-# Define a function to handle user login
 def login():
+
     username = input(USERNAME_MESSAGE)
+
     if not username_exists(username):
-        return 0 # Username not found
+
+        return 0 
+    
     password = input(PASSWORD_MESSAGE)
+
     if password == database[username]["password"]:
-        return username # Login successful
+
+        return username
+    
     else:
+
         return -1 # Incorrect password
 
-# Define a function to handle user registration
+
 def register():
+
+    global username
+
     username = input(USERNAME_MESSAGE)
+
     if username_exists(username):
+
         return -1 # Username already exists
+    
     password = input(PASSWORD_MESSAGE)
+
     secret = input(SECRET_MESSAGE)
+
     database[username] = {"password": password, "secret": secret}
+
     return 1 # Registration successful
 
+def choice():
+
+    print("What would you like to do (^u^) ?")
+    print("=============================")
+    print(LOGIN_CHOICE)
+    print(REGISTER_CHOICE)
+    print(EXIT_CHOICE)
+    v = int(input("> "))
+    print("")
+    return v
 
 
+while True: 
 
-
-
-while True: #loop runs forever till break
-    c = printingOutAllChoicesAndReturnUserChoice()
+    c = choice()
     match c:
         case 1:
 
-            s = fun2()
+            s = login()
+
             if s == 0:
-                print("error: username not found")
+
+                print("error: username not found ('o')")
+
             elif s == -1:
-                print("error: incorrect password")
+
+                print("error: incorrect password (;-;)")
+
             else:
-                print(f"\nWelcome back, {s}!")
+
+                print(f"\nWelcome back \(>w<)/ {username}!")
+
                 print("Your secret phrase is:")
-                print(database[s]["secret"])
+
+                print(database[username]["secret"])
+
             continue
+
         case 2:
-            s=fun1()
+
+            s=register()
+
             if s == 1:
-                print("\nSuccessfuly Registered!")
+
+                print("\nSuccessfuly Registered! (UwU)")
+
             elif s==-1:
-                print("error: username already exists")
+
+                print("error: username already exists ('-')")
+
             continue
+
         case 3:
-            print("Thank You")
+        
+            print("Thank You ('u')")
+
             break
+
         case _:
+
             print("error: invalid input")
